dddbef507e3b30e193d8ebb5307d0d374fa35ed7
SeRealMF <christoph.weichenhain@gmail.com>
2025-03-21 14:01:24 +0100

    Mass tank and insu

---
+++ mass_tank_and_insu.py
@@ -0,0 +1,7 @@
+A_Tank = 1.4599971*10**(9)/(10**6) # m^2
+s_Tank = 1.524/1000 # m
+V_Tank = A_Tank * s_Tank # m^3
+rho_Tank = 1577.75 # kg/m^3
+m_Tank = V_Tank * rho_Tank * 1.05 # kg
+m_Insu = 1803 # kg
+m_T_I_tot = m_Tank + m_Insu # kg
