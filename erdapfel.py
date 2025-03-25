510192fe21c34263638c2b420967db1dc227f410
SeRealMF <christoph.weichenhain@gmail.com>
2025-03-25 19:37:47 +0100

    potatoes for unloading added and algorithm for computation changed

--- erdapfel.py
+++ erdapfel.py
@@ -30,7 +30,6 @@ py.append(m_OE)
 px.append(x_CoG_OE)
 py.append(m_OE + con.m_fStr)
 
-plt.plot(px, py, 'o')
 plt.plot(px, py, '-', label = "Fueling")
 
 # boarding
@@ -51,39 +50,33 @@ py = list()
 px.append(x_CoG_OE)
 py.append(m_OE + con.m_fStr)
 
-x_alt = x_CoG_OE
+x_row = list()
+m_row = list()
 
-for i in range(1, n_row+1):
-    j = n_row - i + 1
-    if j > 31:
-        m_cur = m_OE + con.m_fStr + m_row_econ * i
-        x_cur = x_row_51-pitch_econ*(i-1)
-        x_neu = (x_alt*(m_cur-m_row_econ) + x_cur*m_row_econ)/ m_cur
-        px.append(x_neu)
-        py.append(m_cur)
-        x_alt = x_neu
-    elif j == 31:
-        m_cur = m_OE + con.m_fStr + m_row_econ * i
-        x_cur = x_row_31
-        x_neu = (x_alt*(m_cur-m_row_econ) + x_cur*m_row_econ)/ m_cur
-        px.append(x_neu)
-        py.append(m_cur)
-        x_alt = x_neu
-    elif j < 31 and j > 15:
-        m_cur = m_OE + con.m_fStr + m_row_econ * i
-        x_cur = x_row_30-pitch_econ*(i-1)
-        x_neu = (x_alt*(m_cur-m_row_econ) + x_cur*m_row_econ)/ m_cur
-        px.append(x_neu)
-        py.append(m_cur)
-        x_alt = x_neu
-    elif j <= 15:
-        m_cur = m_OE + con.m_fStr + m_row_econ * 36 + m_row_prem*(i-36)
-        x_cur = x_row_15-pitch_prem*(i-1)
-        x_neu = (x_alt*(m_cur-m_row_econ) + x_cur*m_row_econ)/ m_cur
-        px.append(x_neu)
-        py.append(m_cur)
-    else:
-        print("Wrong row index reached. Please review.")
+for i in range(15):
+    x_row.append(x_row_15-14*pitch_prem+i*pitch_prem)
+    m_row.append(m_row_prem)
+for i in range(15):
+    x_row.append(x_row_30-14*pitch_econ+i*pitch_econ)
+    m_row.append(m_row_econ)
+for i in range(1):
+    x_row.append(x_row_31)
+    m_row.append(m_row_econ)
+for i in range(20):
+    x_row.append(x_row_51-19*pitch_econ+i*pitch_econ)
+    m_row.append(m_row_econ)
+
+x_old = x_CoG_OE
+m_old_it0 = m_OE + con.m_fStr
+
+for i in range(len(x_row)):
+    m_old = m_old_it0 + sum(m_row[50-i:])
+    m_new = m_old + m_row[50-i]
+    x_new = 1/m_new * (x_old * m_old + x_row[50-i] * m_row[50-i])
+    px.append(x_new)
+    py.append(m_new)
+    x_old = x_new
+    m_old = m_new
 
 plt.plot(px, py, "-", label = "Boarding")
 
@@ -92,14 +85,14 @@ plt.plot(px, py, "-", label = "Boarding")
 n_ld = 12
 x_ld = list()
 
-for i in range(1, n_ld+1):
-    x_ld.append(54.786-n_ld*1.536+1.536/2*i)
+for i in range(0, n_ld):
+    x_ld.append(54.786-n_ld*1.536+1.536/2+1.536*i)
 
 m_cargo = 6656 # kg
 m_ld = m_cargo / n_ld
 
-m_old_it0 = m_OE + con.m_fStr + 24564
-x_old = x_neu
+m_old_it0 = m_new
+x_old = x_new
 
 px = [x_old]
 py = [m_old_it0]
@@ -133,4 +126,116 @@ plt.grid(True)
 plt.show()
 
 
+
+
+
+
+
+
+
+
+
 # unloading phase
+
+x_CoG_TO = x_new
+
+# limits
+
+    # max nose gear load
+
+x = np.linspace(28.39, 30.35, 100)
+y = 553928 / (34.828 - x)
+plt.plot(x, y, "-", label = "Max Nose Gear Load")
+
+    # min nose gear load
+
+plt.plot([33.34, 33.34], [m_OE, main.W_Take_off], "-", label = "Min Nose Gear Load")
+
+# unfueling
+
+px = list()
+py = list()
+
+px.append(x_CoG_TO)
+py.append(main.W_Take_off)
+px.append(x_CoG_TO)
+py.append(main.W_Take_off - con.m_fStr)
+
+plt.plot(px, py, '-', label = "Fueling")
+
+# cargo unloading
+
+n_ld = 12
+x_ld = list()
+
+for i in range(0, n_ld):
+    x_ld.append(54.786-n_ld*1.536+1.536/2+1.536*i)
+
+m_cargo = 6656 # kg
+m_ld = m_cargo / n_ld
+
+m_old_it0 = main.W_Take_off - con.m_fStr
+x_old = x_CoG_TO
+
+px = [x_old]
+py = [m_old_it0]
+
+for i in range(len(x_ld)):
+    m_old = m_old_it0 - m_ld*i
+    m_new = m_old - m_ld
+    x_new = 1/m_new * (x_old * m_old - x_ld[n_ld-i-1] * m_ld)
+    px.append(x_new)
+    py.append(m_new)
+    x_old = x_new
+    m_old = m_new
+
+plt.plot(px, py, "-", label = "Cargo Unloading")
+
+# disembarking
+
+m_row_econ = 6*83 + 3*12 # kg
+m_row_prem = 4*83 + 2*12 # kg
+n_row = 15 + 15 + 1 + 20 # amount of rows
+x_row_51 = 52.619 # distance in x direction for row bla from tip
+x_row_31 = 36.034
+x_row_30 = 34.891
+x_row_15 = 19.554
+pitch_econ = 32*2.54/100
+pitch_prem = 36*2.54/100
+
+px = list()
+py = list()
+
+px.append(x_new)
+py.append(m_new)
+
+x_old = x_new
+m_old_it0 = m_new 
+
+for i in range(len(x_row)):
+    m_old = m_old_it0 - sum(m_row[:i])
+    m_new = m_old - m_row[i]
+    x_new = 1/m_new * (x_old * m_old - x_row[i] * m_row[i])
+    px.append(x_new)
+    py.append(m_new)
+    x_old = x_new
+    m_old = m_new
+
+plt.plot(px, py, "-", label = "Disembarking")
+
+# plot
+
+xmin = 27.5
+xmax = 32.5
+ymin = m_OE
+ymax = main.W_Take_off
+plt.xlim(xmin, xmax)
+plt.ylim(ymin, ymax)
+
+plt.xlabel('x (m)')
+plt.ylabel('m (kg)')
+plt.title('Unloading Phase')
+plt.legend()
+plt.grid(True)
+
+plt.show()
