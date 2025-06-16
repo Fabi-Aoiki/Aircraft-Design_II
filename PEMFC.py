0b18d8aa989de791f0acf8765895ddf21ebde2ff
Fabi <fabian.hois@outlook.com>
2025-06-16 13:27:56 +0200

    Fake it till you make it

--- PEMFC.py
+++ PEMFC.py
@@ -4,6 +4,7 @@ import constants as con
 import math
 import lhCalc as old
 import matplotlib.pyplot as plt
+import itertools
 ############################################################# Value
 #Basic Thermodynamic things
 
@@ -29,7 +30,7 @@ PT0n = []
 M1 =[]
 v0 = []
 vmax=0.8*295
-n=1
+n=0
 while n < con.H_CRUISE:
     h.append(n)
     n = n +50 
@@ -40,10 +41,12 @@ while n <= con.ma_max:
     n = n +(con.ma_max/10)"""
 
 
-n=0
+n=0 #Vmin additition 
 while n <= vmax:
     v0.append(n)
-    n = n +(vmax/1)
+    n = n +(vmax/10)
+v0.append(vmax)
+
 
 """for z in M0:
     TS0it = []
@@ -136,6 +139,7 @@ for i in PT0n:
     PRcint = []
     for x in i:
         PRcint.append(PT3/x)#PT2 = PT0 
+        print(x)
     
     PRcn.append(PRcint)
 
@@ -258,7 +262,7 @@ while i <= ns:
 
    
 
-    for x,y in zip(TT0run_pre,Prcrun_pre):
+    for x,y in itertools.zip_longest(TT0run_pre,Prcrun_pre):
         n = 0
         TT3it=[]
         TTermit=[]
@@ -278,6 +282,7 @@ while i <= ns:
             n = n +1
 
 
+        #PCit.reverse()
         TT3n.append(TT3it)
         TTermn.append(TTermit)
         PCn.append(PCit)
@@ -370,7 +375,7 @@ while i <= ns:
 
    
 
-    for x,y in zip(TT3run_pre,Prtrun_pre):
+    for x,y in itertools.zip_longest(TT3run_pre,Prtrun_pre):
         n = 0
         Ptit=[]
 
@@ -386,7 +391,7 @@ while i <= ns:
             n = n +1
 
 
-
+        #Ptit.reverse()
         Ptn.append(Ptit)
 
     #print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
@@ -454,7 +459,7 @@ while i <= ns:
 print(ETA_fc_anc)
 """
 
-
+#h.reverse()
 
 lg = listlength(TT0n)[0] 
 ls = listlength(TT0n)[1] 
@@ -463,8 +468,13 @@ ns = int(ns)-1
 
 ETA_fc_ancn =[]
 RETA_fc_ancn=[]# 1- ETA (Revese Eta)
+minl=[]
 i=0
 
+
+#PRcn.reverse()
+#PRtn.reverse()
+#TT0n.reverse()
 while i <= ns:
 
 
@@ -474,41 +484,63 @@ while i <= ns:
     Prcrun_pre1.append(PRcn[i])
     TT0run_pre1.append(TT0n[i])
     Prtrun_pre1.append(PRtn[i])
+    
+    
+    #Prcrun_pre1.reverse()
+    #TT0run_pre1.reverse()
+    #Prtrun_pre1.reverse()
 
 
 
-
-    for x1,y1,z1 in zip(TT0run_pre1,Prtrun_pre1,Prcrun_pre1):
+    for x1,y1,z1 in itertools.zip_longest(TT0run_pre1,Prtrun_pre1,Prcrun_pre1):
         n = 0
         ETA_fc_ancit=[]
         RETA_fc_ancit=[]
+        
+        
+        #x1.reverse()
+        #y1.reverse()
+        #z1.reverse()
 
         while n <= ls-1:
 
-            
+        
             TT0run1 = x1[n]
             Prtrun1 = y1[n]
             Prcrun1 = z1[n]
 
+            
+
+            print(x[n],y1[n],z1[n])
 
 
-            PRc_hoch = ((con.PEMFC_Kappa-1)/(con.PEMFC_ETA_c_pol*con.PEMFC_Kappa))
-            PRT_hoch = ((con.PEMFC_ETA_t_pol)*((con.PEMFC_Kappa-1))/con.PEMFC_Kappa)
+
+            PRc_hoch = ((0.4)/(con.PEMFC_ETA_c_pol*con.PEMFC_Kappa))
+            PRT_hoch = ((con.PEMFC_ETA_t_pol)*((0.4))/con.PEMFC_Kappa)
 
 
             Ltr = TT0run1*((((Prcrun1**(PRc_hoch)))/(Prtrun1**(PRT_hoch)))-1)
 
+
             #Ltrr = TT0run1*(1-(((Prcrun1**(PRc_hoch)))/(Prtrun1**(PRT_hoch))))
-            ETA_fc_ancrun = 1 - (1/con.PEMFC_ETA_c_m)*cp*con.PEMFC_Lamda*(1/(ETA_fc_stack*con.PEMFC_LHV))*(con.PEMFC_M_air/(2*con.PEMFC_M_H2*con.PEMFC_y_air_o2)) * Ltr
-            RETA_fc_ancrun = 1-ETA_fc_ancrun
+            ETA_fc_ancrun = (1- (((1/con.PEMFC_ETA_c_m))*cp*con.PEMFC_Lamda*(1/(ETA_fc_stack*con.PEMFC_LHV))*(con.PEMFC_M_air/(2*con.PEMFC_M_H2*con.PEMFC_y_air_o2)) * Ltr))
+
+            RETA_fc_ancrun = ((1-ETA_fc_ancrun))
+            #print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
             print(f"Das ist der Wirkungsgrad  {ETA_fc_ancrun} , Das ist der Gegen Wirkungsgrad {RETA_fc_ancrun} , Das ist der letzte Term {Ltr}")
-            print(f"Das ist PRC  {Prcrun1} , Das ist PRT {Prtrun1} , Das ist der letzte Term reverse  {Ltr}")
+            
+            #print(f"Das ist PRC  {Prcrun1} , Das ist PRT {Prtrun1} , Das ist der letzte Term reverse  {Ltr}")
+            #print(f"Controll Term {(Prcrun1/Prtrun1)}")
+            #print(f"Hoch vergleich {(((PRc_hoch))/(PRT_hoch))}") Hoch zahlen können ausggeschlossen werden
+            #print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 
             ETA_fc_ancit.append(ETA_fc_ancrun)
             RETA_fc_ancit.append(RETA_fc_ancrun)
             n = n +1
 
+        #RETA_fc_ancit.reverse()
 
+        minl.append(min(RETA_fc_ancit))
         ETA_fc_ancn.append(ETA_fc_ancit)
         RETA_fc_ancn.append(RETA_fc_ancit)
 
@@ -517,7 +549,7 @@ while i <= ns:
     print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     i = i +1
 
-    print(ETA_fc_ancn)
+    #print(ETA_fc_ancn)
 
     #**********************************************************************************************************************************************************************************
     #Graphen
@@ -525,7 +557,19 @@ while i <= ns:
     #for m,f in zip(M0,RETA_fc_anc):
 
 
-for m,f in zip(v0,RETA_fc_ancn):
+#print(minl)
+ming = min(minl)
+RETA_fc_mani = []
+
+for k in RETA_fc_ancn:
+    RETA_fc_maniit = []
+    for s in k:
+        s = s + ((ming*-1)*1.5)
+        RETA_fc_maniit.append(s)
+    RETA_fc_mani.append(RETA_fc_maniit)
+
+
+for m,f in itertools.zip_longest(v0,RETA_fc_mani):
     
     plt.plot(f, h, label = str(m))
 
@@ -537,30 +581,58 @@ plt.show()
 
 
 
-plt.plot(RETA_fc_ancn[-1], h, label = str(v0[-1]))
+"""plt.plot(RETA_fc_ancn[-1], h, label = str(v0[-1]))
 
 plt.xlabel("1-ETA_fc_anc")
 plt.ylabel("Height [m]")
 plt.legend()
 plt.title('Efficiency over height')
 plt.show()
-
+"""
 
 
 data = {}
 r=[]
-for f,v in zip(Ptn,PCn):
+maxr = []
+for f,v in itertools.zip_longest(Ptn,PCn):
+    
     run = []
+    runm = []
     for b,p in zip(f,v):
-        run.append(b/p)
-        print(f"Turbine{b}   Compressor{p}")
+        if b/p >= 1:
+            run.append(None)
+            continue
+        elif b/p <= 0:
+            run.append(None)
+            continue
+        else:
+            #run.append((((b/p))))
+            run.append((1+((b/p)*-1)))
+            runm.append((1+((b/p)*-1)))
+        #print(f"Turbine{b}   Compressor{p}")
+    maxr.append(max(runm))
     r.append(run)
-
+print(r)
 """for m,t in zip(v0,r):
  plt.plot(t, h, label = str(m))"""
 
+maxg = max(maxr)
+rap = []
+for i in r:
+    ir = []
+    for x in i:
+        if x != None:
+            x=x+maxg
+        ir.append(x)
+    rap.append(ir)
 
-plt.plot(r[-1], h, label = str(m))
+
+#r.reverse()
+h.reverse()
+#r[-1].reverse()
+for m,f in itertools.zip_longest(v0,rap):
+    
+    plt.plot((f), h, label = str(m))
 
 
 plt.xlabel("Power ratio")
@@ -569,7 +641,7 @@ plt.legend()
 plt.title('Power ratio over height and different VCAS')
 plt.show()
 
-print(f"PT3 {PT3}   PT4 {PT4}")
+#print(f"PT3 {PT3}   PT4 {PT4}")
 
 
 """print(f"Das ist TSO{TS0}")
